c5c9469b6063902f19354a806ac3e5fc5f71f90d
vkyuen <vkyuen@mail.sfsu.edu>
2018-05-10 18:46:44 -0700

    gradientDiffusion setup

--- GradientDiffusion.cpp
+++ GradientDiffusion.cpp
@@ -1,2 +1,14 @@
+#include "gradientDiffusion.h"
 // Based on example code from https://itk.org/ITKExamples/src/Filtering/AnisotropicSmoothing/ComputeGradientAnisotropicDiffusion/Documentation.html?highlight=anisotropic
 
+// note for time step, need to use something smaller than .12207 for .dcm files
+gradientDiffusion::gradientDiffusion(){
+// by default, assuming dicom folder is in the same folder as the source file
+  
+} // end gradientDiffusion()
+  gradientDiffusion::gradientDiffusion(char* dirName){
+
+  }// end gradienDiffusion(char* dirName)
+  void gradientDiffustion::applyFilter(){
+
+  }// end applyFilter
