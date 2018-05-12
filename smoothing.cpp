038af37e4d1831519644afd6679b8c9b100743b2
vkyuen <vkyuen@mail.sfsu.edu>
2018-05-11 19:15:56 -0700

    finished binary thresholding

--- smoothing.cpp
+++ smoothing.cpp
@@ -6,6 +6,10 @@
 #include "itkImage.h"
 #include "itkBinaryThresholdImageFilter.h"
 
+
+
+smoothing::applySmoothing(ReaderType::Pointer reader){ 
+
 using PixelType = unsigned char;
 constexpr unsigned int Dimension = 3; 
 
@@ -13,9 +17,9 @@ using ImageType = itk::Image< PixelType, Dimension>;
 using ReaderType = itk::itkImageFileReader<ImageType>;
 
 using BinaryThresholdFilterType = itk::itkBinaryThresholdImageFilter< ImageType, ImageType>;
+
 using WriterType = itk::ImageFileWriter< ImageType >;
 
-smoothing::applyBinaryThresholding(char* inImage){ 
   // ttps://itk.org/ITKExamples/src/Filtering/Thresholding/ThresholdAnImageUsingBinary/Documentation.html?highlight=threshold%20binary
 
   auto lowerThresh = static_cast<PixelType>(30);
@@ -23,28 +27,15 @@ smoothing::applyBinaryThresholding(char* inImage){
   auto OutsideValue = static_cast<PixelType>(0);
   auto InsideValue = static_cast<PixelType>(255);
 
-  ReaderType::Pointer reader = ReaderType::New();
-  reader->SetFileName( inImage);
+  // ReaderType::Pointer reader = ReaderType::New();
+  // reader->SetFileName( dicom);
 
-  BinaryThresholdFilterType::Pointer filter = BinaryThresholdFilterType::New();
-  filter->SetInput( reader->GetOutput() );
-  filter->SetLowerThreshold( LowerThreshold );
-  filter->SetUpperThreshold( UpperThreshold );
-  filter->SetOutsideValue( OutsideValue );
-  filter->SetInsideValue( InsideValue );
+  BinaryThresholdFilterType::Pointer binaryFilter = BinaryThresholdFilterType::New();
+  binaryFilter->SetInput( reader->GetOutput() );
+  binaryFilter->SetLowerThreshold( LowerThreshold );
+  binaryFilter->SetUpperThreshold( UpperThreshold );
+  binaryFilter->SetOutsideValue( OutsideValue );
+  binaryFilter->SetInsideValue( InsideValue );
 
-  WriterType::Pointer writer = WriterType::New();
-  writer->SetFileName( outImage );
-  writer->SetInput( filter->GetOutput() );
-
-  try
-    {
-    writer->Update();
-    }
-  catch( itk::ExceptionObject & e )
-    {
-    std::cerr << "Error: " << e << std::endl;
-    return EXIT_FAILURE;
-    }
 
 }// end applyBinaryThresholding
