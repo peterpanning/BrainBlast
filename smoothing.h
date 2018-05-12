29d54e87542b6e850d1e05a471b31a00a0a023eb
vkyuen <vkyuen@mail.sfsu.edu>
2018-05-11 20:52:15 -0700

    added smoothing.h

---
+++ smoothing.h
@@ -0,0 +1,23 @@
+#ifndef FINALPROJECT_SMOOTHING_H 
+#define FINALPROJECT_SMOOTHING_H
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkImage.h"
+#include "itkBinaryThresholdImageFilter.h"
+#include "itkGradientAnisotropicDiffusionImageFilter.h"
+#include "itkRescaleIntensityImageFilter.h"
+
+class smoothing{
+  // need to define the new types
+  constexpr unsigned int Dimension = 3; 
+  using PixelType = unsighend char;
+  using ImageType = itk::Image< PixelType, Dimension>;
+  using ReaderType = itk::itkImageFileReader<ImageType>;
+  using GradientFilterType = itk::itkGradientAnisotropicDiffusionImageFilter<ImageType, ImageType>;
+  using RescaleType = itk::RescaleIntensityImgeFilter<ImageType, ImageType>;
+  using WriterType = itk::ImageFileWriter< ImageType >;
+
+public:
+  applySmoothing(ReaderType::point);
+};
